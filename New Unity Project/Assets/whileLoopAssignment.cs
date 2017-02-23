7979a3016eff907cc4c6c6396d382eb24c8e8506
Unknown <akprice95@msn.com>
2017-02-22 23:03:47 -0700

    Assignment for while loop

---
+++ New Unity Project/Assets/whileLoopAssignment.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whileLoopAssignment : MonoBehaviour
+{
+
+    private int minValue = -10;
+    private int maxValue = 10;
+    private int randomNum ;
+    private int counter = 0;
+    private int secretNum = 4;
+    // Use this for initialization
+    void Start()
+    {
+
+        while (randomNum != secretNum)
+        {
+            counter++;
+            randomNum = Random.Range(minValue, maxValue);
+            secretNum = 5;
+            if (randomNum == secretNum)
+            {
+
+                print(" Your Secret Number is " + secretNum);
+                print(counter);
+                break;
+            }
+
+
+
+
+        }
+    }
+}
+
+		
+	
+	
+	
+		
+
+
---
+++ New Unity Project/Assets/whileLoopInClass.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class whileLoopInClass : MonoBehaviour
+{
+    public int bottles = 0;
+    private bool gameOver = true;
+    public int maxBottles = 10;
+    // Use this for initialization
+    void Start()
+    {
+        /*  while (bottles < 10)
+          {
+              print(bottles + "bottles of root beer on the wall. ");
+              bottles = bottles + 2;
+          }
+          print("all done"); */
+
+        while (gameOver == true)
+        {
+            print(bottles + " bottles of rootbeer on the wall. ");
+            bottles++;
+            if (bottles == maxBottles)
+            {
+                gameOver = false;
+            }
+
+
+        }
+    }
+}
