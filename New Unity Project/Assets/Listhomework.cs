eea16acb59a71a077f29ffd8ed043379b3db0fb9
Unknown <akprice95@msn.com>
2017-03-01 20:51:52 -0700

    Assignment for Animal List

---
+++ New Unity Project/Assets/Listhomework.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listhomework : MonoBehaviour
+{
+
+    public string animal;
+    public List<string> zooRoster = new List<string>();
+
+
+    // Use this for initialization
+    void Start()
+    {
+        zooRoster.Add("monkey");
+        zooRoster.Add("Shark");
+        zooRoster.Add("Elephant");
+        zooRoster.Add("alligator");
+        zooRoster.Add("Tarzan");
+        zooRoster.Add("Lion");
+
+    }
+    public void addAnimal()
+    {
+        if (zooRoster.Contains(animal) != true)
+            if (animal != "")
+            {
+                if (zooRoster.Contains(animal) != true)
+                {
+                    zooRoster.Add(animal);
+                }
+
+
+
+
+            }
+    }
+
+    public void removeAnimal(int listIndex)
+    {
+        zooRoster.RemoveAt(listIndex);
+    }
+
+
+
+
+    
+    
+    public void clearAnimal()
+    {
+        zooRoster.Clear();
+    }
+
+    public void printAnimals()
+    {
+        print(zooRoster);
+    }
+    
+
+}
+
---
+++ New Unity Project/Assets/Listinclass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listinclass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        print("------------");
+        CountToTen();
+        print("------------");
+        CountToTen();
+        print("------------");
+        CountToTen();
+        
+    }
+
+    void CountToTen()
+    {
+        int i = 1;
+        while (i <= 10)
+        {
+            print(i);
+            i++;
+        }
+        print ("Congrats! You counted to 10!");
+            
+
+    }
+
+}
